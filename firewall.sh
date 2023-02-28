ee242baa8c08189f2ccfa8f8c3cc4bcbde3056b5
Mike Soskin <62444444+S0sk1nDoesD3v@users.noreply.github.com>
2023-02-27 18:58:05 -0600

    Create firewall.sh

---
+++ firewall.sh
@@ -0,0 +1,23 @@
+# My system IP/set ip address of server here
+SERVER_IP="54.83.170.202"
+ 
+# Flushing all rules
+iptables -F
+iptables -X
+ 
+# Setting default filter policy
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -P FORWARD DROP
+ 
+# Allow unlimited traffic on loopback
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+ 
+# Allow incoming ssh only
+iptables -A INPUT -p tcp -s 0/0 -d $SERVER_IP --sport 513:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -s $SERVER_IP -d 0/0 --sport 22 --dport 513:65535 -m state --state ESTABLISHED -j ACCEPT
+ 
+# make sure nothing comes or goes out of this box
+iptables -A INPUT -j DROP
+iptables -A OUTPUT -j DROP
